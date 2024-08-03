60d35432f6245426e8c90e82790ac88ce58847f6
ro <robylee27@gmail.com>
2024-08-03 09:05:15 -0500

    housekeeping

--- scripts/p4abswp.py
+++ scripts/p4abswp.py
@@ -3,8 +3,8 @@ while not name:
     print('Enter your name')
     name = input()
 print('How many guests will you have?')
-numOfGuests = input()
-if numOfGuests:
+num_of_guests = input()
+if num_of_guests:
         print('Be sure you have enough room for all your guests.')
 print('Done')
 
