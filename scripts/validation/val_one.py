79b878e7df6e97cb786f6f5fa096c7c03367dd1b
roronoaBoi <roby.lee@safetyculture.io>
2024-10-01 05:46:28 -0500

    added pw example loop

--- scripts/validation/val_one.py
+++ scripts/validation/val_one.py
@@ -2,8 +2,8 @@
 basics of PyInputPlus in a venv
 """
 #pylint: disable=import-error
-#added the above disble... as this import is working correctly
-import pyinputplus as pyip
+#added the above disble and the below type ignore as this import is working correctly
+import pyinputplus as pyip # type: ignore
 
 print('type a number')
 
