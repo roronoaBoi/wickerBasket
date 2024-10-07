c715c2210a378bc1a7cc14c4b4359631441809fa
roronoaBoi <roby.lee@safetyculture.io>
2024-10-07 16:28:24 -0500

    dynamic len

--- scripts/lma.py
+++ scripts/lma.py
@@ -5,9 +5,27 @@ dont ask
 import random
 import time
 
+processes = [
+    'deleting item ',
+    'truncating the flux data on record ',
+    'callibrating vpn for record ',
+    'de-centralizing load balancers on record ',
+    'copying and pasting record '
+]
+
+ports = [
+    '1522',
+    '2309',
+    '5434',
+    '1432',
+    '0800'
+]
+
 while True:
     record = random.randint(10000000,70000000)
-    print('processing record ' + str(record) + ' through api port 1522...')
+    to_proc = random.choice(processes)
+    to_port = random.choice(ports)
+    print(to_proc + str(record) + ' through api port ' + to_port + '...')
     time.sleep(3)
     print('completed record ' + str(record) + '!')
     print({
